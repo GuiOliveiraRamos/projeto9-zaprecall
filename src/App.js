73f92723179eb91733c59058597c860475090c34
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-02-28 16:16:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/reset.css"
+import "./css/style.css"
+import logo from "./assets/logo.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titulo">
+        <img src={logo} alt={logo}></img>
+        <h1>ZapRecall</h1>
+      </div>
+      <div></div>
     </div>
   );
 }
