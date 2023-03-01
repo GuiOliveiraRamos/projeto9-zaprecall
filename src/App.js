cf4f356e93f8c022c5b812ad323fcd70a207d27b
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-03-01 10:56:12 -0300

    implementando style-components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import "./css/reset.css"
 import "./css/style.css"
 import logo from "./assets/logo.png"
+import Perguntas from "./Perguntas";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <div className="titulo">
+    <><Appstyled>
+      <Titulo>
         <img src={logo} alt={logo}></img>
         <h1>ZapRecall</h1>
-      </div>
-      <div></div>
-    </div>
-  );
+      </Titulo>
+      <Perguntas />
+    </Appstyled>
+      <Footer>
+        <p>0/4 CONCLUÍDOS</p>
+      </Footer></>
+  )
 }
 
 export default App;
+
+
+const Appstyled = styled.div`
+    width: 375px;
+    height: 1000px;
+    background-color: #FB6B6B;
+    display: flex;
+    align-items: center;
+    flex-direction: column;     
+    padding-top: 42px;
+`
+
+const Titulo = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 51px;
+
+    h1 {
+    font-family:'righteous';
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: #FFFFFF;
+}
+img {
+    width: 52px;
+    height: 60px;
+    padding-right: 20px;
+}
+`
+
+const Footer = styled.div`
+    width: 375px;
+    height: 70px;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+
+    p {
+    font-family: 'Recursive';
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+}
+`
